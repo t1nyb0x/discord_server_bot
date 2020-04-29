c7493fcd7297a051a24e5b54d03cbb73e8b3d8b5
t1nyb0x <dev.t1nyb0x@gmail.com>
2020-04-29 13:20:53 +0900

    インデント修正

--- dokachan.py
+++ dokachan.py
@@ -11,8 +11,11 @@ class JapaneseHelpCommand(commands.DefaultHelpCommand):
 
     def get_ending_note(self):
         return (f"channels: このサーバーに存在するチャンネルを紹介するぜ\n"
+                f"\n"
                 f"trans: 岡山の県北で培った語学力で翻訳するぜ。 /dokachan trans <翻訳先言語> <翻訳したい内容> で実行するんや\n"
-                f"翻訳可能言語は、 /dokachan translist で確認してくれ。\n"
+                f"\n"
+                f"translist: 岡山で学んだ翻訳可能言語を紹介するぜ。\n"
+                f"\n"
                 f"help: 今開いている内容を出すぜ\n")
 
 
@@ -26,7 +29,8 @@ class Dokachan(commands.Cog):
         self.bot = bot
 
     def translate(self, lang, source):
-        """translate
+        """
+        translate
 
         Arguments:
             lang   {string} -- 翻訳先言語
@@ -40,25 +44,29 @@ class Dokachan(commands.Cog):
 
     def translateList(self):
         res = ''
-        res = """このサーバーでよく見かける言語に変換する場合はこれを使ってね。
-                英語  en
-                韓国語 ko
-                スペイン語 es
-                日本語 ja
-                他の言語はこちらを参照してください https://cloud.google.com/translate/docs/languages?hl=ja"""
+        res = """
+            このサーバーでよく見かける言語に変換する場合はこれを使ってね。
+            英語  en
+            韓国語 ko
+            スペイン語 es
+            日本語 ja
+            他の言語はこちらを参照してください https://cloud.google.com/translate/docs/languages?hl=ja
+            """
 
         return res
 
     def channelList(self):
         res = ''
-        res = """welcome - ランディングチャンネル
-                チャンネル説明 - このサーバーに関する説明が掲載されています
-                告知 - このサーバーに関する告知や、個人に関する告知が掲載されます
-                自己紹介はこちらへ - サーバーに入ったらまずは自己紹介！
-                雑談 - 多分ここがメインチャンネル。好きなようにお喋りしましょう。喧嘩はご法度です
-                プログラム - Dokachanやyattazeを作った人がここで何かしらやってます。Botへの追加機能要望を出すのもありかも？
-                お問い合わせ窓口 - サーバーに関して気になることがあればこちらへ
-                麻雀 - 麻雀打ちたい人はこちらへ。大抵雀魂で打ってます"""
+        res = """
+            welcome - ランディングチャンネル
+            チャンネル説明 - このサーバーに関する説明が掲載されています
+            告知 - このサーバーに関する告知や、個人に関する告知が掲載されます
+            自己紹介はこちらへ - サーバーに入ったらまずは自己紹介！
+            雑談 - 多分ここがメインチャンネル。好きなようにお喋りしましょう。喧嘩はご法度です
+            プログラム - Dokachanやyattazeを作った人がここで何かしらやってます。Botへの追加機能要望を出すのもありかも？
+            お問い合わせ窓口 - サーバーに関して気になることがあればこちらへ
+            麻雀 - 麻雀打ちたい人はこちらへ。大抵雀魂で打ってます
+            """
 
         return res
 
