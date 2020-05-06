9586991cbaeaab6bef675622d24d472e5360801c
t1nyb0x <dev.t1nyb0x@gmail.com>
2020-05-07 08:09:37 +0900

    primary_areaデータ取得

---
+++ weather.py
@@ -0,0 +1,17 @@
+import requests
+from pprint import pprint
+import urllib.parse
+
+class Weather():
+    def __init__(self):
+        self.api = 'http://api.aitc.jp/jmardb-api/search?'
+    
+    def search(self, location, dateFrom, dateTo):
+        url = self.api + 'datetime=' + dateFrom + 'datetime=' + dateTo + 'title=' + urllib.parse.quote(location)
+        pprint(url)
+        res = requests.get(url)
+        return res
+
+
+test = Weather()
+pprint(test.search('府県天気概況', '2020-05-06', '2020-05-06'))
