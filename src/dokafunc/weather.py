16cc7f86dacec61fc7b2b24010703a5be69e54cf
t1nyb0x <dev.t1nyb0x@gmail.com>
2020-10-08 23:32:13 +0900

    天気予報機能テスト

--- src/dokafunc/weather.py
+++ src/dokafunc/weather.py
@@ -1,5 +1,4 @@
 import requests
-from pprint import pprint
 import json
 
 class Weather():
@@ -8,12 +7,9 @@ class Weather():
     """
     def __init__(self, token):
         self.api = 'http://api.openweathermap.org/data/2.5/weather?appid=' + token
-        # self.api = 'http://api.openweathermap.org/data/2.5/weather?appid=ba39a2a0a9e627c2390fd27f7b60213b'
-    
+
     def search(self, cityName):
         url = self.api + '&q=' + cityName
-        pprint(url)
         res = requests.get(url)
         data = res.json()
-        pprint(data)
         return data
