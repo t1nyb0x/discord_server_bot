c2910cf4bc377fa2d0a27a0f0f46120044c93c2e
t1nyb0x <dev.t1nyb0x@gmail.com>
2021-10-17 14:19:19 +0900

    ログイン処理作成

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
-import { REST } from '@discordjs/rest';
+// import { REST } from '@discordjs/rest';
 import dotenv from 'dotenv';
+import { Client, Intents } from 'discord.js';
+
 dotenv.config();
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 const discordToken = process.env.TOKEN;
 
-if (!discordToken) {
-    throw new Error('Discord Token is missing.');
-}
-const rest = new REST({ version: '9' }).setToken(discordToken);
-console.log(rest);
+client.on('ready', () => {
+    if (client.user === null) {
+        throw new Error('Failed get client');
+    }
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()) return;
+
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Pong!');
+    }
+});
+
+client.login(discordToken);
