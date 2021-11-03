551a472fb01ecc0eee2715fd50f4b6c24cc0ded0
t1nyb0x <dev.t1nyb0x@gmail.com>
2021-11-03 13:16:13 +0900

    スペース取得処理作成

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import { Client, Intents } from 'discord.js';
 import { Translate } from './modules/translate/translate';
+import { TwitterSpace } from './modules/space/space';
 
 const prefix = '>>';
 dotenv.config();
@@ -17,15 +18,17 @@ if (discordToken == undefined) {
     throw new Error('Failed get discordToken');
 }
 
-client.on('ready', () => {
+client.on('ready', async () => {
     if (client.user === null) {
         throw new Error('Failed get client');
     }
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('messageCreate', (m) => {
+client.on('messageCreate', async (m) => {
     if (!m.content.startsWith(prefix) || m.author.bot) return;
+
+    if (m.content.length === 0) return;
 
     const args = m.content.slice(prefix.length).trim().split(/ +/);
     const command = args.shift()?.toLowerCase();
@@ -33,6 +36,29 @@ client.on('messageCreate', (m) => {
     if (command === 'translatelist') {
         const translate = new Translate();
         m.channel.send(translate.translateList());
+    }
+
+    if (command === 'supervise_spaces') {
+        const twitterSpace = new TwitterSpace(process.env.TWITTER_API_BEARER);
+        // ユーザー名を指定して、スペースの検索を行う
+        const res = await twitterSpace.search(args[0]);
+
+        if (res === undefined) return;
+
+        const ctxText = `スペースが現在開催中です。
+        
+タイトル：${res.title}
+https://twitter.com/i/spaces/${res.spaceId}
+
+開始時間： ${res.createdAt}
+現在${String(res.participantCount)}人が参加中です
+スピーカー
+\`\`\`
+${res.speakerUsers.join('\n')}
+\`\`\`
+        `;
+
+        m.channel.send(ctxText);
     }
 });
 
