a1c312d3c96ff37d2e4efff0c9410221e6dcfa03
t1nyb0x <dev.t1nyb0x@gmail.com>
2022-09-15 19:29:14 +0900

    fix server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 const app = express();
+import { InteractionType, InteractionResponseType, verifyKeyMiddleware } from 'discord-interactions';
 require('./main');
 
 const PORT = process.env.PORT || 3000;
+const PUBLIC_KEY = process.env.PUBLIC_KEY;
 
 app.get('/', (req, res) => {
     res.send('Welcome!');
@@ -11,3 +13,21 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${PORT}`);
 })
+
+if (PUBLIC_KEY) {
+    app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async(req, res) => {
+        const interaction = req.body;
+
+        if (interaction.type === InteractionType.APPLICATION_COMMAND) {
+            console.log(interaction.data.name);
+            // if (interaction.data.name === 'yo') {
+            //     return res.send({
+            //         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            //         data: {
+            //             content: `Yo ${interaction.member.user.username}`,
+            //         },
+            //     });
+            // }
+        }
+    });
+}
