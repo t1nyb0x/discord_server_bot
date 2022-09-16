079bb35ad4fb3d26383bd26c99630bd3a1e70cdb
t1nyb0x <dev.t1nyb0x@gmail.com>
2022-09-16 10:54:37 +0900

    interactionsのreturn値設定

---
+++ src/server.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+const app = express();
+import { InteractionType, InteractionResponseType, verifyKeyMiddleware } from 'discord-interactions';
+require('./main');
+
+const PORT = process.env.PORT || 3000;
+const PUBLIC_KEY = process.env.PUBLIC_KEY;
+
+app.get('/', (req, res) => {
+    res.send('Welcome!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${PORT}`);
+})
+
+if (PUBLIC_KEY) {
+    app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async(req, res) => {
+        const interaction = req.body;
+
+        if (interaction.type === InteractionType.APPLICATION_COMMAND) {
+            console.log(interaction.data.name);
+            // if (interaction.data.name === 'yo') {
+            //     return res.send({
+            //         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            //         data: {
+            //             content: `Yo ${interaction.member.user.username}`,
+            //         },
+            //     });
+            // }
+        }
+
+        return res.send({
+            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            data: {
+                content: 'OK'
+            }
+        });
+    });
+}
